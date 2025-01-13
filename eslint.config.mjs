de4e7a47ce96475c0993c1803d2cb051a3095cd8
shadil-rayyan <shadilrayyan2@gmail.com>
2025-01-13 13:02:36 +0000

    test after commitlin and husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,43 +1,58 @@
-import { FlatCompat } from '@eslint/eslintrc';
-import js from '@eslint/js';
+import antfu from '@antfu/eslint-config';
+import nextPlugin from '@next/eslint-plugin-next';
+import jestDom from 'eslint-plugin-jest-dom';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import playwright from 'eslint-plugin-playwright';
+import tailwind from 'eslint-plugin-tailwindcss';
+import testingLibrary from 'eslint-plugin-testing-library';
 
-const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
+export default antfu({
+  react: true,
+  typescript: true,
+
+  lessOpinionated: true,
+  isInEditor: false,
+
+  stylistic: {
+    semi: true,
+  },
+
+  formatters: {
+    css: true,
+  },
+
+  ignores: [
+    'migrations/**/*',
+    'next-env.d.ts',
+  ],
+}, ...tailwind.configs['flat/recommended'], jsxA11y.flatConfigs.recommended, {
+  plugins: {
+    '@next/next': nextPlugin,
+  },
+  rules: {
+    ...nextPlugin.configs.recommended.rules,
+    ...nextPlugin.configs['core-web-vitals'].rules,
+  },
+}, {
+  files: [
+    '**/*.test.ts?(x)',
+  ],
+  ...testingLibrary.configs['flat/react'],
+  ...jestDom.configs['flat/recommended'],
+}, {
+  files: [
+    '**/*.spec.ts',
+    '**/*.e2e.ts',
+  ],
+  ...playwright.configs['flat/recommended'],
+}, {
+  rules: {
+    'antfu/no-top-level-await': 'off', // Allow top-level await
+    'style/brace-style': ['error', '1tbs'], // Use the default brace style
+    'ts/consistent-type-definitions': ['error', 'type'], // Use `type` instead of `interface`
+    'react/prefer-destructuring-assignment': 'off', // Vscode doesn't support automatically destructuring, it's a pain to add a new variable
+    'node/prefer-global/process': 'off', // Allow using `process.env`
+    'test/padding-around-all': 'error', // Add padding in test files
+    'test/prefer-lowercase-title': 'off', // Allow using uppercase titles in test titles
+  },
 });
-
-const eslintConfig = [
-  ...compat.config({
-    extends: [
-      'plugin:@next/next/recommended', // Extend Next.js recommended rules
-      'plugin:@typescript-eslint/recommended',
-      'plugin:prettier/recommended', // Extend TypeScript rules
-    ],
-    parser: '@typescript-eslint/parser', // Use TypeScript parser
-    parserOptions: {
-      ecmaVersion: 2020,
-      sourceType: 'module',
-      ecmaFeatures: {
-        jsx: true, // Enable JSX parsing
-      },
-    },
-    ignorePatterns: [
-      '**/node_modules/**', // Ignore node_modules directory
-      '**/.next/**', // Ignore Next.js build directory
-      '**/build/**', // Ignore build directory if applicable
-      '**/public/**', // Ignore public directory if applicable
-      '**/*.log', // Ignore log files
-    ],
-    rules: {
-      curly: 'error', // Require curly braces for all control statements
-      quotes: ['error', 'single'], // Enforce single quotes
-      semi: ['error', 'always'], // Require semicolons
-      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }], // Disallow unused variables, allowing ignored ones
-      'prefer-const': ['error'], // Prefer const over let
-      'no-var': ['error'], // Disallow var
-      'max-len': ['error', { code: 80 }], // Limit maximum line length to 80 characters
-      indent: ['error', 2], // Enforce consistent indentation of 2 spaces
-    },
-  }),
-];
-
-export default eslintConfig;
