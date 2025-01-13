f0e9404491f3d606a896718af2143044cee3917c
shadil-rayyan <shadilrayyan2@gmail.com>
2025-01-13 12:35:27 +0000

    added the eslitn pretteri andit seem to be owrking

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,7 +9,8 @@ const eslintConfig = [
   ...compat.config({
     extends: [
       'plugin:@next/next/recommended', // Extend Next.js recommended rules
-      'plugin:@typescript-eslint/recommended', // Extend TypeScript rules
+      'plugin:@typescript-eslint/recommended',
+      'plugin:prettier/recommended', // Extend TypeScript rules
     ],
     parser: '@typescript-eslint/parser', // Use TypeScript parser
     parserOptions: {
@@ -21,20 +22,20 @@ const eslintConfig = [
     },
     ignorePatterns: [
       '**/node_modules/**', // Ignore node_modules directory
-      '**/.next/**',        // Ignore Next.js build directory
-      '**/build/**',        // Ignore build directory if applicable
-      '**/public/**',       // Ignore public directory if applicable
-      '**/*.log'           // Ignore log files
+      '**/.next/**', // Ignore Next.js build directory
+      '**/build/**', // Ignore build directory if applicable
+      '**/public/**', // Ignore public directory if applicable
+      '**/*.log', // Ignore log files
     ],
     rules: {
-      'curly': 'error', // Require curly braces for all control statements
-      'quotes': ['error', 'single'], // Enforce single quotes
-      'semi': ['error', 'always'], // Require semicolons
-      'no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }], // Disallow unused variables, allowing ignored ones
+      curly: 'error', // Require curly braces for all control statements
+      quotes: ['error', 'single'], // Enforce single quotes
+      semi: ['error', 'always'], // Require semicolons
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }], // Disallow unused variables, allowing ignored ones
       'prefer-const': ['error'], // Prefer const over let
       'no-var': ['error'], // Disallow var
-      'max-len': ['error', { 'code': 80 }], // Limit maximum line length to 80 characters
-      'indent': ['error', 2], // Enforce consistent indentation of 2 spaces
+      'max-len': ['error', { code: 80 }], // Limit maximum line length to 80 characters
+      indent: ['error', 2], // Enforce consistent indentation of 2 spaces
     },
   }),
 ];
