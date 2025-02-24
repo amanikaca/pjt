a64e4007103c99780ec9fc21c872d916f3c5295f
SHADIL A M <shadilrayyan2@gmail.com>
2025-02-24 21:45:06 +0530

    pwa problem solved build failed 2nd time retrying again

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,6 +1,7 @@
 // app/manifest.ts
 import type { MetadataRoute } from 'next';
 
+export const dynamic = 'force-static';
 export default function manifest(): MetadataRoute.Manifest {
   return {
     name: 'Gecian Hub',
