f95d5d3ee2693dd1bb3bb8842cd43470b007ac89
shadil-rayyan <shadilrayyan2@gmail.com>
2025-05-26 20:03:08 +0530

    fix(bus&club) missed the code in club slug

--- src/app/club/slug/page.tsx
+++ src/app/club/slug/page.tsx
@@ -1,27 +1,17 @@
 // app/bus/[busNumber]/page.tsx
 
 
-export async function generateStaticParams() {
-  return [
-    { slug: '1' },
-    { slug: '2' },
-    { slug: '3' },
-    // Add more known slugs/bus numbers
-  ];
-}
-
-export default function BusPage({ params }: { params: { slug: string } }) {
+export default function Home() {
   return (
     <div>
-      <h1>Bus Number: {params.slug}</h1>
       <p>
-        In this dynamic page, the UI/UX remains consistent across all buses, but
-        the content updates based on the bus number.
+        In this dynamic page, when you click any bus button, the UI/UX for all bus locations remains the same. 
+        We use a dynamic page system so that clicking on, for example, the "Bus 1" button redirects here, 
+        but the displayed content updates to show the respective bus's location.
       </p>
     </div>
   );
 }
-
 
 // import { notFound } from 'next/navigation';
 
