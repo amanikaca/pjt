2d88140dc7b0f1d973d8038486328e53f2ca4dc4
shadil-rayyan <shadilrayyan2@gmail.com>
2025-05-26 20:18:55 +0530

    fix(build):problem did not have output:export in nextjs config so local npm run build did not have probelm but github action did have problem in it so multiple commit to fix the issue was needed

---
+++ next.config.js
@@ -0,0 +1,8 @@
+// next.config.js
+const nextConfig = {
+    // Remove or comment this if you’re not doing a full static export
+    // output: 'export'
+  };
+  
+  module.exports = nextConfig;
+  
