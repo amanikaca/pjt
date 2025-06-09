3f291cfb4309c752459b9f1fd9e95642e6066db2
Fahad <mofahadkp@proton.me>
2025-06-09 11:34:44 +0530

    Fixed top and bottom nav bar

--- src/components/topNavbar.tsx
+++ src/components/topNavbar.tsx
@@ -1,31 +1,35 @@
-'use client';
+"use client";
 
-import { faCircleUser } from '@fortawesome/free-solid-svg-icons/faCircleUser';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import { User } from "iconsax-react";
+import React from "react";
 
-const Header: React.FC<{ userName: string }> = ({ userName }) => {
+const Header: React.FC<{
+  userName: string;
+  semester: number;
+  department: string;
+}> = ({ userName, semester, department }) => {
   return (
-    <div className="flex items-center justify-between rounded-lg bg-background px-4 py-2 shadow-md dark:border-gray-700 md:border md:border-gray-300 md:shadow-none">
+    <div className="flex items-center justify-between px-4 py-7 bg-gradient-to-b from-transparent to-20% to-[var(--main)] shadow-[0_8px_20px_10px_var(--main)]">
       {/* User Name */}
-      <span className="truncate text-lg font-bold text-foreground">
-        {userName || 'Guest'}
-      </span>
+      <div className="flex flex-col">
+        <p className="truncate text-2xl text-foreground">
+          Hello, <span className="font-bold">{userName || "Guest"}</span>
+        </p>
+        <p className="truncate text-md text-foreground">
+          {semester && "S" + String(semester) + ","} {department || "CSE"}
+        </p>
+      </div>
 
       {/* User Icon */}
       <button
         type="button"
-        className="flex size-10 items-center justify-center rounded-full bg-purple-200 transition-colors hover:bg-purple-300 dark:bg-purple-800 dark:hover:bg-purple-700"
+        className="flex size-10 items-center justify-center rounded-full bg-gray-600 transition-colors hover:bg-purple-300 dark:bg-purple-800 dark:hover:bg-purple-700"
         title="User Profile"
-        onClick={() => console.warn('Profile Clicked')} // Changed to warn
+        onClick={() => console.warn("Profile Clicked")} // Changed to warn
         aria-label="Open user profile"
       >
-        <FontAwesomeIcon
-          icon={faCircleUser}
-          className="size-5 text-purple-800 dark:text-purple-200"
-        />
+        <User size="28" color="white" variant="Linear" />
       </button>
-
     </div>
   );
 };
